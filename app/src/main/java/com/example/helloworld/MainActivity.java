2fa86c066a302b91a93aa5811945400db5e95953
vijay shekhar <vijay.shekhar463@gmai.com>
2021-01-26 19:56:21 +0530

    safety check using toast. state transitions remaining.

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,35 +1,117 @@
 package com.example.helloworld;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button submitbt, clearbt;
     EditText nameet;
+    CheckBox c1,c2,c3,c4,c5;
+
+    private int reqKey =0;
+    private boolean checkClicked=false;
 
     // on create state
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         submitbt = (Button)findViewById(R.id.submit_button);
+        clearbt = (Button)findViewById(R.id.clear_button);
         nameet = (EditText)findViewById(R.id.name);
+        c1 = (CheckBox)findViewById(R.id.prec1);
+        c2 = (CheckBox)findViewById(R.id.prec2);
+        c3 = (CheckBox)findViewById(R.id.prec3);
+        c4 = (CheckBox)findViewById(R.id.prec4);
+        c5 = (CheckBox)findViewById(R.id.prec5);
+
+
+
 
         submitbt.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 String msg = nameet.getText().toString();
                 Intent intent = new Intent(MainActivity.this , SecondActivity.class);
-                intent.putExtra("message",msg);
-                startActivity(intent);
+                intent.putExtra("user name",msg);
+
+                boolean c1check, c2check, c3check, c4check, c5check;
+                c1check = c1.isChecked();
+                c2check = c2.isChecked();
+                c3check = c3.isChecked();
+                c4check = c4.isChecked();
+                c5check = c5.isChecked();
+
+
+                intent.putExtra("c1" , c1check);
+                intent.putExtra("c2" , c2check);
+                intent.putExtra("c3" , c3check);
+                intent.putExtra("c4" , c4check);
+                intent.putExtra("c5" , c5check);
+
+//                String s1=String.valueOf(c1check);
+//                String s2=String.valueOf(c2check);
+//                String s3=String.valueOf(c3check);
+//                String s4=String.valueOf(c4check);
+//                String s5=String.valueOf(c5check);
+
+//                Toast.makeText(MainActivity.this, s1+" "+s3+" "+s4+" "+s5 , Toast.LENGTH_SHORT).show();
+
+                startActivityForResult(intent , reqKey);
             }
         });
+
+        clearbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                c1.setChecked(false);
+                c2.setChecked(false);
+                c3.setChecked(false);
+                c4.setChecked(false);
+                c5.setChecked(false);
+                nameet.setText("");
+                nameet.setHint("Enter your name");
+
+            }
+        });
+
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode!= Activity.RESULT_OK)
+            return;
+        if(requestCode==reqKey){
+            if(data==null){
+                return;
+            }
+            checkClicked= data.getBooleanExtra("safe_msg",false);
+
+            if(checkClicked){
+                Toast.makeText(MainActivity.this, "You are safe!!!", Toast.LENGTH_LONG).show();
+            }
+            else{
+                Toast.makeText(MainActivity.this, "You are NOT SAFE!!!", Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/helloworld/SecondActivity.java
+++ app/src/main/java/com/example/helloworld/SecondActivity.java
@@ -1,23 +1,84 @@
 package com.example.helloworld;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
 
+    Button checkbt;
+    CheckBox c1,c2,c3,c4,c5;
+    EditText et;
+    boolean msg;
+    private boolean safe;
+    boolean c1check, c2check, c3check, c4check, c5check;
+    private final static String DEBUG_TAG="Second Activity";
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        String str = getIntent().getExtras().getString("message");
 
-        Toast tost = Toast.makeText(getApplicationContext(), str, Toast.LENGTH_LONG);
-        tost.show();
+//        initializing checkboxes
+        checkbt = (Button)findViewById(R.id.check_button);
+        c1 = (CheckBox)findViewById(R.id.prec1);
+        c2 = (CheckBox)findViewById(R.id.prec2);
+        c3 = (CheckBox)findViewById(R.id.prec3);
+        c4 = (CheckBox)findViewById(R.id.prec4);
+        c5 = (CheckBox)findViewById(R.id.prec5);
 
+        et = (EditText)findViewById(R.id.namereg);
+
+
+//        retrieving checkbox values from previous activity
+
+        c1check = this.getIntent().getBooleanExtra("c1", false);
+        c2check = this.getIntent().getBooleanExtra("c2", false);
+        c3check = this.getIntent().getBooleanExtra("c3", false);
+        c4check = this.getIntent().getBooleanExtra("c4", false);
+        c5check = this.getIntent().getBooleanExtra("c5", false);
+
+        String namereg = this.getIntent().getStringExtra("user name");
+
+//      assigning checked value to the checkboxes in another activity.
+        c1.setChecked(c1check);
+        c2.setChecked(c2check);
+        c3.setChecked(c3check);
+        c4.setChecked(c4check);
+        c5.setChecked(c5check);
+
+        et.setText(namereg);
+
+
+    }
+
+
+
+    public void checkStatus(View v){
+        safe = c1check && c2check && c3check && c4check && c5check;
+
+                if(safe) {
+                    msg = true;
+                }
+                else{
+                    msg = false;
+                }
+
+        Log.d(DEBUG_TAG, "you have been checked");
+        Intent results = new Intent();
+        results.putExtra("safe_msg" , msg);
+        setResult(Activity.RESULT_OK,results);
     }
 }
