ec8acf0e6a3775482d70ad3b5923a4eeae21fe0d
vijay shekhar <vijay.shekhar463@gmai.com>
2021-02-14 15:43:37 +0530

    code reformatting.

--- app/src/main/java/com/example/helloworld/Activities/DownloadMusicActivity.java
+++ app/src/main/java/com/example/helloworld/Activities/DownloadMusicActivity.java
@@ -36,31 +36,77 @@ public class DownloadMusicActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_download_music);
+
+
         statusText = findViewById(R.id.status_msg);
         internetStatus = findViewById(R.id.inernet_status_msg);
         downloadStatus = findViewById(R.id.download_status_msg);
+
+
         internetStatus.setText("Checking Internet Status...");
-        if (haveNetworkConnection()) {
+
+        // if internet connection is available
+        if (connectionEstablished()) {
             internetStatus.setText("Internet Connection Established...");
-            //continue
-            new DownloadWebpageTask().execute("http://faculty.iiitd.ac.in/~mukulika/s1.mp3");
+            //start downloading task
+            new SongDownloadTask().execute("http://faculty.iiitd.ac.in/~mukulika/s1.mp3");
         } else {
+
             internetStatus.setText("No Internet Connection...");
             statusText.setText("Can't proceed with download");
             downloadStatus.setText("Can't proceed with download");
-//            this.finish();
+
         }
     }
 
 
-    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+    // Checking the connection availability
+    public boolean connectionEstablished() {
+
+        // constants for mobile connection and wifi connection
+        boolean mobileconnection_status = false, wificonnection_status = false, status = false;
+
+        // class objects
+        ConnectivityManager conObjc = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkStatus = conObjc.getActiveNetworkInfo();
+
+
+        if (networkStatus != null) {
+            // connected to the internet
+            switch (networkStatus.getType()) {
+
+                // case for mobile data
+                case ConnectivityManager.TYPE_MOBILE:
+                    mobileconnection_status = true;
+                    break;
+                // case for wifi connection
+                case ConnectivityManager.TYPE_WIFI:
+                    wificonnection_status = true;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        // if any mode of connection is available
+        return  mobileconnection_status || wificonnection_status;
+
+    }
+
+
+    // download song using Async task <-- Asyc taks is depricated-- >
+    private class SongDownloadTask extends AsyncTask<String, Void, String> {
+
+
         @Override
         protected String doInBackground(String... urls) {
             try {
                 Log.i("DEBUG_TAG", "in doInBackground" + urls[0]);
-                return downloadUrl(urls[0]);
+
+                return downloadSongLink(urls[0]);
             } catch (IOException e) {
-                return "Unable to retrieve web page. URL may be invalid.";
+                return "Check the url, cannot download song: "+ urls[0];
             }
         }
 
@@ -84,73 +130,71 @@ public class DownloadMusicActivity extends AppCompatActivity {
     }
 
 
-    private String downloadUrl(String myurl) throws IOException {
-        InputStream is = null;
+
+
+
+    private String downloadSongLink(String dataLink) throws IOException {
+        InputStream ipStream = null;
         statusText.setText("Starting Download...");
+
         try {
+
             Log.d("DEBUG_TAG", "In download url: ");
-            URL url = new URL(myurl);
-            HttpURLConnection conn = (HttpURLConnection)
-                    url.openConnection();
-            conn.setReadTimeout(10000 /* milliseconds
-             */);
-            conn.setConnectTimeout(15000 /* milliseconds */);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true);
+            URL link = new URL(dataLink);
+
+            HttpURLConnection conObj = (HttpURLConnection) link.openConnection();
+
+            conObj.setRequestMethod("GET");
+
+            conObj.setConnectTimeout(21000 );
+            conObj.setReadTimeout(21000 );
+
+
+            conObj.setDoInput(true);
+
+
             // Starts the query
-            conn.connect();
-            int response = conn.getResponseCode();
-            if(response==200){
+            conObj.connect();
+
+            int response = conObj.getResponseCode();
+            if (response == 200) {
                 statusText.setText("Connection Established...");
             }
 
-            downloadStatus.setText("File Downloaded and saved as myfile.mp3");
-            Log.d("DEBUG_TAG", "The response is: " + response);
-            is = conn.getInputStream();
-            // write inputStream into file
-            String filename = "myfile.mp3";
-            FileOutputStream fos = openFileOutput(filename, Context.MODE_PRIVATE);
-            byte[] buffer = new byte[4096];
-            int len1 = 0;
+            downloadStatus.setText("Starting download ");
+            Log.d("DEBUG_TAG", "The response received: " + response);
 
-            while ((len1 = is.read(buffer)) != -1) {
-                fos.write(buffer, 0, len1);
+            // static data initialisations
+            String fname = "song_download.mp3";  // file name
+            byte[] data = new byte[4096];   // raw data
+            int l = 0;  // count variable
+
+            // input stream data
+            ipStream = conObj.getInputStream();
+
+            // writing data from input stream into file song_download
+            FileOutputStream fileopStream = openFileOutput(fname, Context.MODE_PRIVATE);
+
+            // looping for writing data
+            while ((l = ipStream.read(data)) != -1) {
+                fileopStream.write(data, 0, l);
             }
-            // Makes sure that the InputStream is closed after the app is
-            // finished using it.
-            String[] files = DownloadMusicActivity.this.fileList();
-            Log.d("DEBUG_TAG", files[0]);
-            downloadStatus.setText("File Downloaded and saved as myfile.mp3");
+
+            String [] dataList = DownloadMusicActivity.this.fileList();
+            Log.d("DEBUG_TAG", dataList[0]);
+
+            downloadStatus.setText("File Downloaded");
             return "success";
+
         } finally {
-            if (is != null) {
-                is.close();
+            if (ipStream != null) {
+                ipStream.close();
             }
         }
     }
 
 
-    public boolean haveNetworkConnection() {
-        boolean haveConnectedWifi = false;
-        boolean haveConnectedMobile = false;
 
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        if (netInfo != null) {
-            // connected to the internet
-            switch (netInfo.getType()) {
-                case ConnectivityManager.TYPE_WIFI:
-                    haveConnectedWifi = true;
-                    break;
-                case ConnectivityManager.TYPE_MOBILE:
-                    haveConnectedMobile = true;
-                    break;
-                default:
-                    break;
-            }
-        }
-        return haveConnectedWifi || haveConnectedMobile;
-    }
 
 
 }
