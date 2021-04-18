3804f05e802e8a348b7cd385fc34de6cf5c105a4
vijay shekhar <vijay.shekhar463@gmai.com>
2021-04-18 13:03:23 +0530

    single point location done.

--- app/src/main/java/com/example/helloworld/Activities/LocationActivity.java
+++ app/src/main/java/com/example/helloworld/Activities/LocationActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld.Activities;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
+import androidx.room.RoomDatabase;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -11,6 +12,8 @@ import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,8 +45,10 @@ public class LocationActivity extends AppCompatActivity {
 
     List<ScanResult> scanResultsMain;
     String location = "";
-    int rss1=0,rss2=0;
+    int rss1 = 0, rss2 = 0;
+    static List<WifiEntity1> dataDownload;
     private static final String TAG = "LocationActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,96 +64,156 @@ public class LocationActivity extends AppCompatActivity {
 
         WifiManager wifimanager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
+        final WifiDB wifiDB = Room.databaseBuilder(LocationActivity.this, WifiDB.class, "WifiDataDB").build();
+
+        // switch on the wifi sensor
+        wifimanager.setWifiEnabled(true);
+
+        // intent filter for broadcast receiver
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        scanStatus.setText("Scan Status: Scanning ....");
+
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+
+                scanResultsMain = wifimanager.getScanResults();
+                final int n = scanResultsMain.size();
+
+                Log.d(TAG, "Wifi Scan Results Count: " + n);
+                Toast.makeText(LocationActivity.this, "No. of results:  " + n, Toast.LENGTH_SHORT).show();
+                scanStatus.setText("Scan Status: Scanning Complete!");
+
+                for (ScanResult res : scanResultsMain) {
+                    int signalStrength = wifimanager.calculateSignalLevel(res.level);
+                    Log.d(TAG, " SSID  =  " + res.SSID);
+                    Log.d(TAG, " BSSID  =  " + res.BSSID);
+                    Log.d(TAG, " level (RSS)  =  " + (res.level + 100));
+                    Log.d(TAG, " strength  =  " + signalStrength);
+
+                    if (res.SSID.contains("naraya_vani")) {
+                        rss1 = res.level + 100;
+                    }
+                    if (res.SSID.contains("uamsef")) {
+                        rss2 = res.level + 100;
+                    }
+
+                }
+            }
+
+        }, intentFilter);
+        wifimanager.startScan();
+
+
+        //thread to download data from room database
+        APInfoRetrive apInfoRetrive = new APInfoRetrive(LocationActivity.this, wifiDB);
+        apInfoRetrive.start();
+//            Toast.makeText(this, "data size: "+dataDownload.size(), Toast.LENGTH_SHORT).show();
+
+
+//            finalResult arr[] = new finalResult[dataDownload.size()];
+//            int i=0;
+//            for (WifiEntity1 res: dataDownload){
+//                arr[i].roomName = res.getRoomName();
+//                int temp = (res.getRssi1() - rss1) * res.getRssi1() - rss1;
+//                temp =  temp + ((res.getRssi1() - rss1) * (res.getRssi1() - rss1));
+//                arr[i].dist = (int) Math.sqrt(temp);
+//            }
+//            Compare obj = new Compare();
+//
+//            location = obj.compare(arr, dataDownload.size());
+//            Toast.makeText(this, "room location--->"+location, Toast.LENGTH_SHORT).show();
+
 
         bt1.setOnClickListener(v -> {
+        TextView tv = findViewById(R.id.textview);
+        tv.setText(location);
 
-            // switch on the wifi sensor
-            wifimanager.setWifiEnabled(true);
-
-            // intent filter for broadcast receiver
-            IntentFilter intentFilter = new IntentFilter();
-            intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-            scanStatus.setText("Scan Status: Scanning ....");
-
-            registerReceiver(new BroadcastReceiver() {
-                @Override
-                public void onReceive(Context context, Intent intent) {
-
-
-
-                    scanResultsMain = wifimanager.getScanResults();
-                    final int n = scanResultsMain.size();
-
-                    Log.d(TAG, "Wifi Scan Results Count: " + n);
-                    Toast.makeText(LocationActivity.this, "No. of results:  " + n, Toast.LENGTH_SHORT).show();
-                    scanStatus.setText("Scan Status: Scanning Complete!");
-
-                    for (ScanResult res : scanResultsMain) {
-                        int signalStrength = wifimanager.calculateSignalLevel(res.level);
-                        Log.d(TAG, " SSID  =  " + res.SSID);
-                        Log.d(TAG, " BSSID  =  " + res.BSSID);
-                        Log.d(TAG, " level (RSS)  =  " + (res.level + 100));
-                        Log.d(TAG, " strength  =  " + signalStrength);
-
-                        if(res.SSID.contains("naraya_vani")){
-                            rss1 = res.level + 100;
-                        }
-                        if(res.SSID.contains("uamsef")){
-                            rss2 = res.level + 100;
-                        }
-
-                    }
-                }
-
-            }, intentFilter);
-            wifimanager.startScan();
-
-            WifiDB wifiDB = Room.databaseBuilder(LocationActivity.this, WifiDB.class, "WifiDataDB" ).allowMainThreadQueries().build();
-            List<WifiEntity1> dataDownload=  wifiDB.dao().getAll1();
+//            WifiDB wifiDB = Room.databaseBuilder(LocationActivity.this, WifiDB.class, "WifiDataDB" ).allowMainThreadQueries().build();
+//            List<WifiEntity1> dataDownload=  wifiDB.dao().getAll1();
 //            APInfoRetrive apInfoRetrive = new APInfoRetrive(LocationActivity.this, wifiDB);
 //            apInfoRetrive.start();
-            Toast.makeText(this, "data size: "+dataDownload.size(), Toast.LENGTH_SHORT).show();
-
-
-            finalResult arr[] = new finalResult[dataDownload.size()];
-            int i=0;
-            for (WifiEntity1 res: dataDownload){
-                arr[i].roomName = res.getRoomName();
-                int temp = (res.getRssi1() - rss1) * res.getRssi1() - rss1;
-                temp =  temp + ((res.getRssi1() - rss1) * (res.getRssi1() - rss1));
-                arr[i].dist = (int) Math.sqrt(temp);
-            }
-            Compare obj = new Compare();
-
-            location = obj.compare(arr, dataDownload.size());
-            Toast.makeText(this, "room location--->"+location, Toast.LENGTH_SHORT).show();
+//            Toast.makeText(this, "data size: "+dataDownload.size(), Toast.LENGTH_SHORT).show();
+//
+//
+//            finalResult arr[] = new finalResult[dataDownload.size()];
+//            int i=0;
+//            for (WifiEntity1 res: dataDownload){
+//                arr[i].roomName = res.getRoomName();
+//                int temp = (res.getRssi1() - rss1) * res.getRssi1() - rss1;
+//                temp =  temp + ((res.getRssi1() - rss1) * (res.getRssi1() - rss1));
+//                arr[i].dist = (int) Math.sqrt(temp);
+//            }
+//            Compare obj = new Compare();
+//
+//            location = obj.compare(arr, dataDownload.size());
+//            Toast.makeText(this, "room location--->"+location, Toast.LENGTH_SHORT).show();
 
         });
 
 
-
     }
+
     class Compare {
 
-         String compare(finalResult arr[], int n)
-        {
+        String compare(finalResult arr[], int n) {
             // Comparator to sort the pair according to second element
             Arrays.sort(arr, new Comparator<finalResult>() {
-                @Override public int compare(finalResult p1, finalResult p2)
-                {
+                @Override
+                public int compare(finalResult p1, finalResult p2) {
                     return p1.dist - p2.dist;
                 }
             });
 
-            return arr[n -1].roomName;
+            return arr[n - 1].roomName;
 
         }
     }
 
 
-    public void wifiInfoFetched(List<WifiEntity1> wifiEntity1s){
+    public void wifiInfoFetched(List<WifiEntity1> wifiEntity1s) {
 
-        Log.d(TAG, "wifiInfoFetched: "+wifiEntity1s.size());
-        Toast.makeText(this, "data size: "+wifiEntity1s.size(), Toast.LENGTH_SHORT).show();
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+
+//                this.dataDownload = wifiEntity1s;
+
+                Log.d(TAG, "wifiInfoFetched: " + wifiEntity1s.size());
+                Toast.makeText(LocationActivity.this, "data size: param" + wifiEntity1s.size(), Toast.LENGTH_SHORT).show();
+
+//                Toast.makeText(LocationActivity.this, "data size: global" + dataDownload.size(), Toast.LENGTH_SHORT).show();
+
+
+                finalResult arr[] = new finalResult[wifiEntity1s.size()];
+//                arr = WifiEntity1;
+                int i = 0;
+                for (WifiEntity1 res : wifiEntity1s) {
+                    ;
+//                    arr[i].roomName = res.getRoomName();
+//                    Log.d(TAG, "run: "+ res.getRoomName());
+
+                    String locat = res.getRoomName();
+                    int temp = (res.getRssi1() - rss1) * res.getRssi1() - rss1;
+
+
+                    temp = temp + ((res.getRssi1() - rss1) * (res.getRssi1() - rss1));
+                    arr[i] = new finalResult(locat , (int) Math.sqrt(temp));
+                    i = i+1;
+//                    arr[i].dist = (int) Math.sqrt(temp);
+                }
+                Compare obj = new Compare();
+
+                location = obj.compare(arr, wifiEntity1s.size());
+                Toast.makeText(LocationActivity.this, "room located" + location, Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+
+
     }
+
 }
